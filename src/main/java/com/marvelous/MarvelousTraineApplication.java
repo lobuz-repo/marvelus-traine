cc518fcfcd8391b6817016d5949ccd9127605ac1
lobuz <lobuz>
2019-03-21 17:52:15 +0100

    first commit

---
+++ src/main/java/com/marvelous/MarvelousTraineApplication.java
@@ -0,0 +1,13 @@
+package com.marvelous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelousTraineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelousTraineApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/marvelous/api/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.marvelous.api.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+
+    @GetMapping
+    public String test(){
+        log.info("dziala");
+        return "dupa";
+    }
+
+}
